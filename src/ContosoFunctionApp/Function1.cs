cb308f8c797e81b3bf61ec02374d3d5d385b9577
Daniel Mossberg <5992827+dmossberg@users.noreply.github.com>
2022-09-20 15:10:24 +0200

    Change to contoso app

--- src/ContosoFunctionApp/Function1.cs
+++ src/ContosoFunctionApp/Function1.cs
@@ -12,7 +12,7 @@ namespace ContosoFunctionApp
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("Contoso")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
